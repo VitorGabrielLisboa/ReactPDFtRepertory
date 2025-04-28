4b65bb3fce4c801be841e8e038a1f7d305cdc45d
Vitor Gabriel Lisboa <vitorgabriellisboaferreira@gmail.com>
2025-04-27 23:56:03 -0300

    Conserto de erros dos pdfs

--- src/components/Data/projectsData.js
+++ src/components/Data/projectsData.js
@@ -2,11 +2,11 @@ export const projectsData = [
   {
     id: 1,
     title: "Grandes Mestres do Estoicismo",
-    file: "/pdfs/SerEstoico.pdf",
+    file: process.env.PUBLIC_URL + "/pdfs/projetoX.pdf",
   },
   {
     id: 2,
     title: "Box Grandes Mestres do Estoicismo-Edipro (2021)",
-    file: "/pdfs/MestresDoEstoicismo.pdf",
+    file: process.env.PUBLIC_URL + "/pdfs/MestresDoEstoicismo.pdf",
   },
 ];
